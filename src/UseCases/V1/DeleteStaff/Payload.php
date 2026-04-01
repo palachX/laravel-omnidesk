6590050959b0c36bfe3bc892eb5720c2363544bd
Alexey Gursky <gurski.alex@gmail.com>
2026-04-01 12:56:56 +0300

    feat(v1.6.1): update payload delete and disable staff

--- src/UseCases/V1/DeleteStaff/Payload.php
+++ src/UseCases/V1/DeleteStaff/Payload.php
@@ -7,12 +7,13 @@ namespace Palach\Omnidesk\UseCases\V1\DeleteStaff;
 use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use Spatie\LaravelData\Optional;
 
 #[MapName(SnakeCaseMapper::class)]
 final class Payload extends Data
 {
     public function __construct(
         public readonly int $staffId,
-        public readonly DeleteStaffData $staff
+        public readonly DeleteStaffData|Optional $staff = new Optional
     ) {}
 }
--- src/UseCases/V1/DisableStaff/Payload.php
+++ src/UseCases/V1/DisableStaff/Payload.php
@@ -7,11 +7,12 @@ namespace Palach\Omnidesk\UseCases\V1\DisableStaff;
 use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use Spatie\LaravelData\Optional;
 
 #[MapName(SnakeCaseMapper::class)]
 final class Payload extends Data
 {
     public function __construct(
-        public readonly DisabledStaffData $staff
+        public readonly DisabledStaffData|Optional $staff = new Optional
     ) {}
 }
